eb600fa120e43c9062ce5f04c53e8c5a556c4963
JasonBian <imaginezz@outlook.com>
2016-03-30 16:32:00 +0800

    update HitMouse game

---
+++ UnityBuild/HitMouse/Assets/Assets/Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.skin.button.fontSize = Screen.height/15;
+		GUI.skin.button.normal.textColor = Color.red;
+		if(GUI.Button(new Rect(Screen.width/6, Screen.height/6, Screen.width*4/6, Screen.height*4/6), 
+				"Start Game!")){
+			Application.LoadLevel(1);
+		}
+	}
+}
